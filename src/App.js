383ff2c8011cff00cc1280fbbe656297c98d6d13
jhbansal-deloitte <jhbansal@deloitte.com>
2022-12-22 16:36:22 +0530

    signup/login

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import Otp from "./Components/OTP/Otp";
 import Background from "./Components/Background/Background";
 import Reset from "./Components/Reset/Reset";
-
+import Signup from "./Components/Signup/Signup";
+import SignIn from "./Components/SignIn/SignIn";
+import Forgotpass from "./Components/ForgotPass/Forgotpass";
 function App() {
   return (
     <div className="App">
@@ -11,6 +13,9 @@ function App() {
         <Routes>
           <Route exact path = "/otp" element = {<Otp></Otp>}></Route>
           <Route exact path = "/reset-password" element = {<Reset></Reset>}></Route>
+          <Route exact path = "/signup" element = {<Signup></Signup>}></Route>
+          <Route exact path = "/signin" element = {<SignIn></SignIn>}></Route>
+          <Route exact path = "/fp" element = {<Forgotpass></Forgotpass>}></Route>
         </Routes>
       </Router>
       
